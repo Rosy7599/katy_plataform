7401ff25f57fef0ac58cdfe045c7397b4b95d7fe
Rosalia_Archundia <rosaly.her1534@gmail.com>
2022-06-10 15:32:18 -0500

    cambio en js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const campoNombre = document.getElementById('nombre');
-const campoEmpresa = document.getElementById('empresa');
-const campoEmail = document.getElementById('email');
-const campoDireccion = document.getElementById('direccion');
-const formularioCliente = document.getElementById('form');
+//event
+document.getElementById("form").addEventListener("submit", crear);
 
-const mensajeTabla = document.getElementById('ventanaCliente');
-const tablaCliente = document.getElementById('tablaCliente');
+// crear
 
-let clientes = [];
+function crear(e)
+{
+    empresa = document.getElementById("empresa").value
+    nombre = document.getElementById("nombre").value
+    email = document.getElementById("email").value
+    direccion = document.getElementById("direccion").value
 
-console.log(nombre);
+    let clientes = {
+        empresa,
+        nombre,
+        email,
+        direccion
+    }
+    if(localStorage.getItem("clientes") === null){
+        let clientes = []
+        clientes.push(clientes)
+        localStorage.setItem("clientes", JSON.stringify(clientes))
+        }else{
+            let clientes = JSON.parse(localStorage.getItem("clientes"))
+            clientes.push(cliente)
+            localStorage.setItem("clientes", JSON.stringify(clienyes))
+        }
+
+        document.getElementById("form").reset();
+        e.preventDefaul()
+}
