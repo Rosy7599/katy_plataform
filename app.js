cc9c73309a2c0e5a62a5bbb0c38ed85dd48c5161
Rosalia_Archundia <rosaly.her1534@gmail.com>
2022-06-09 21:08:14 -0500

    primer cambio

---
+++ app.js
@@ -0,0 +1,12 @@
+const campoNombre = document.getElementById('nombre');
+const campoEmpresa = document.getElementById('empresa');
+const campoEmail = document.getElementById('email');
+const campoDireccion = document.getElementById('direccion');
+const formularioCliente = document.getElementById('form');
+
+const mensajeTabla = document.getElementById('ventanaCliente');
+const tablaCliente = document.getElementById('tablaCliente');
+
+let clientes = [];
+
+console.log(nombre);
