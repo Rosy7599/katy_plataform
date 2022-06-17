0e60abe499683a9012588d980f26daf7c3eb6fff
Rosalia_Archundia <rosaly.her1534@gmail.com>
2022-06-17 15:48:45 -0500

    cambios ud

--- app.js
+++ app.js
@@ -3,37 +3,42 @@ document.getElementById("form").addEventListener("submit", crear);
 
 // crear
 
-function crear(e)
-{
-   
-    empresa = document.getElementById("empresa").value
-    nombre = document.getElementById("nombre").value
-    email = document.getElementById("email").value
-    direccion = document.getElementById("direccion").value
+function crear() {
+  const empresa = document.getElementById("empresa").value
+  const nombre = document.getElementById("nombre").value
+  const email = document.getElementById("email").value
+  const direccion = document.getElementById("direccion").value
 
-    let cliente = {
-        empresa,
-        nombre,
-        email,
-        direccion
-    }
-    if(localStorage.getItem("clientes") === null){
-        let clientes = []
-        clientes.push(cliente)
-        localStorage.setItem("clientes", JSON.stringify(clientes))
-        }else{
-            let clientes = JSON.parse(localStorage.getItem("clientes"))
-            clientes.push(cliente)
-            localStorage.setItem("clientes", JSON.stringify(clientes))
-        }
-        
+  let cliente = {
+      empresa,
+      nombre,
+      email,
+      direccion
+  }
+      if  ($(empresa && nombre && email && direccion).text() == "") {
+      alert('Tos los campos son requeridos');
+      return false;
+  }
+
+  if (localStorage.getItem("clientes") === null) {
+      let clientes = []
+      clientes.push(cliente)
+      localStorage.setItem("clientes", JSON.stringify(clientes))
+
+  } else {
+      let clientes = JSON.parse(localStorage.getItem("clientes"))
+      clientes.push(cliente)
+      localStorage.setItem("clientes", JSON.stringify(clientes))
+  }
+};
+
+
+       
         leer();
         document.getElementById("form").reset();
         console.log("Cliente guardado correctamente") 
-        e.preventDefault()
+
         
-          
-}
 
 //read
 function leer(){
@@ -62,76 +67,77 @@ function leer(){
        `
     }
 }
-//update
-function editar(nombre){
-    let clientes = JSON.parse(localStorage.getItem("clientes"));
-        for (let i=0; i<clientes.length; i++){
-        if(clientes[i].nombre === nombre){
-            document.getElementById("body").innerHTML = `
-            <div class="row">
-            <main class="container">
-               <h2> Editar Cliente>
-               <form>
-                  <div class="col-auto">
-                   
-                     <div class="col-auto">
-                        <label for="nombre" class="visually-hidden">Nombre</label>
-                         <input type="text" class="form-control" id="newnombre" placeholder="${clientes[i].nombre}">
-                      </div>
-                      <div>
-                        <label for="empresa" class="visually-hidden">Empresa</label>
-                        <input type="text" class="form-control" id="newempresa" placeholder="${clientes[i].empresa}">
-                      </div>
-      
-                      <div class="col-auto">
-                        <label for="email" class="visually-hidden">Email</label>
-                        <input type="email" class="form-control" id="newemail" marcador de posición =" Correo "
-                         patrón =" .+@gmail.com, @hotmail.com, @yahoo.com " placeholder="${clientes[i].email}">
-                      </div>
+//editar cliente
+function editarCliente(indice) {
+  let clientes = JSON.parse(localStorage.getItem("clientes"));
+  document.getElementById("body").innerHTML = `
+          <div class="row">
+          <main class="container">
+             <h2> Editar Cliente </h2>
+             <form >
+                   <div class="col-auto">
+                      <label for="nombre" class="visually-hidden">Nombre</label>
+                       <input type="text" class="form-control" id="newnombre" placeholder="${clientes[indice].nombre}">
+                    </div>
+                    <div>
+                      <label for="empresa" class="visually-hidden">Empresa</label>
+                      <input type="text" class="form-control" id="newempresa" placeholder="${clientes[indice].empresa}">
+                    </div>
     
-                      <div class="col-auto">
-                        <label for="tetx" class="visually-hidden">Direccion</label>
-                        <input type="text" class="form-control" id="newdireccion"  placeholder="${clientes[i].direccion}">
-                      </div>
-                                   
-                  </form>
-                  <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
+                    <div class="col-auto">
+                      <label for="email" class="visually-hidden">Email</label>
+                      <input type="email" class="form-control" id="newemail" marcador de posición =" Correo "
+                       patrón =" .+@gmail.com, @hotmail.com, @yahoo.com " placeholder="${clientes[indice].email}">
+                    </div>
+  
+                    <div class="col-auto">
+                      <label for="tetx" class="visually-hidden">Direccion</label>
+                      <input type="text" class="form-control" id="newdireccion"  placeholder="${clientes[indice].direccion}">
+                    </div>
+                    
+                    </form>
+                    <button class="btn btn-success" onclick="actualizar(${indice})">Actualizar</button>
+  
+                    <button class="btn btn-primary" onclick="vistaPrincipal()">Cancelar</button>
+          </main> 
+     </div>
+      
+   `
 
-                  <button class="btn btn-primary" onclick="vistaPrincipal()">Cancelar</button>
-            </main> 
-       </div>
-        
-     `
 
-        }
-
-    }
 }
 
 //update
-function actualizar(i){
-    let libros = JSON.parse(localStorage.getItem("clientes"));
-    clientes[i].empresa = document.getElementById("newempresa").value;
-    clientes[i].nombre = document.getElementById("newnombre").value;
-    clientes[i].email = document.getElementById("newemail").value;
-    clientes[i].direccion = document.getElementById("newdireccion").value;
-      
-    localStorage.setItem("clientes", JSON.stringify(clientes));
-    vistaPrincipal()
+function actualizar(indice) {
+  const newempresa = document.getElementById("newempresa").value;
+  const newnombre = document.getElementById("newnombre").value;
+  const newemail = document.getElementById("newemail").value;
+  const newdireccion = document.getElementById("newdireccion").value;
+  if (newempresa && newnombre && newemail && newdireccion) {
+      let clientes = JSON.parse(localStorage.getItem("clientes"));
+          clientes[indice] = {
+          newempresa,
+          newnombre,
+          newemail,
+          newdireccion,
+      }
+      localStorage.setItem("clientes", JSON.stringify(clientes));
+      vistaPrincipal()
+  } else {
+      alert('Todos los campos son requeridos');
+  }
 }
 //delete
 
-function eliminar(titulo){
-    let clientes = JSON.parse(localStorage.getItem("clientes"));
-    for(let i=0; i<clientes.length; i++){
-        if(clientes[i].titulo == titulo){
-            clientes.splice(i,1);
-            }
-        }
-       localStorage.setItem("clientes", JSON.stringify(clientes)); 
-       leer();
-    }
-
+const  eliminarCliente  =  ( indice )  =>  {
+   if  ( confirm ( '¿Desea eliminar el elemento?' ) )  {
+    clientes.splice(indice, 1);
+      leer ( ) ;
+      alerta ( 'Se eliminó correctamente.' )
+  }
+} ;
+       
+  
     function vistaPrincipal(){
         document.getElementById("body").innerHTML = `
         <div class="row">
@@ -166,27 +172,8 @@ function eliminar(titulo){
                 </form>
             </main> 
        </div>
-       <section>
-            <table class="table table-dark table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Empresa</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Direccion</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody id="tbody">
-                    <th colspan="5">
-                        <h1 class="mensaje">Sin registros</h1>
-                    </th >
-                </tbody>
-            </table>
-        </section>
-        `
+              `
+
         leer();
     }
     leer();
-
-
